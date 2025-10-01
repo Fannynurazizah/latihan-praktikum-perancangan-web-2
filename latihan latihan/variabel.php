a979862141b7faadfbdfa1b001b981a7ce56a944
Fannynurazizah <fannynur2005@gmail.com>
2025-10-01 11:33:33 +0700

    Create variabel.php

---
+++ latihan latihan/variabel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title> Contoh skrip PHP </title>
+</head>
+<body>
+    <?php
+    $nama = "Fanny Nur Azizah";
+    echo "$nams berkata \"Katakan namamu!\"";
+    ?>
+</body>
+</html>
