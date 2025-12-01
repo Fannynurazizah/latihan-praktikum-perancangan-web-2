9122ab353402ecabb981c77edb35c65a71252861
Fannynurazizah <fannynur2005@gmail.com>
2025-12-01 20:35:52 +0700

    Delete Praktikum_7 directory

--- Praktikum_7/Codingan/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "sewa_layos_dan_sound");
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
-
--- Praktikum_7/codingan/cek_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['id_user'])) {
-    echo "<script>alert('Silakan login dulu.'); window.location='../login.php';</script>";
-    exit;
-}
-?>
